8c49c9fd2166e3e9700d209c3c698c5eee677ca8
KingCQ <wangchengqvan@hotmail.com>
2017-02-19 18:49:41 +0800

    pod update

---
+++ AppBase/BaseExt/Any+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Any+Extension.swift
+//  Elegant
+//
+//  Created by KingCQ on 2017/2/18.
+//  Copyright © 2017年 KingCQ. All rights reserved.
+//
+
+import Foundation
+
+extension NSObject {
+    func toString() -> String {
+        return String(describing: self)
+    }
+}
