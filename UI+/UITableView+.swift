038d8ea94997b23ad21a0ace0e1abf3581d4bc67
Steve <chengquan.wang@ele.me>
2017-05-22 17:54:10 +0800

    extension for UITableView

---
+++ UI+/UITableView+.swift
@@ -0,0 +1,20 @@
+//
+//  UITableView+.swift
+//  Elegant
+//
+//  Created by Steve on 2017/5/22.
+//  Copyright © 2017年 KingCQ. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+
+    ///  Deselects rows. This method will not call the delegate methods (-tableView:didSelectRowAtIndexPath:), nor will it send out a notification.
+    ///
+    /// - Parameter animated: Animation
+    public func deselectAll(animated: Bool) {
+        _ = indexPathsForSelectedRows?.map { deselectRow(at: $0, animated: animated) }
+    }
+
+}
