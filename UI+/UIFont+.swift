58994c3e162cd8b5bd9997a523459f12102d93bc
Steve <chengquan.wang@ele.me>
2017-05-22 17:39:09 +0800

    extension of the  UIFont

---
+++ UI+/UIFont+.swift
@@ -0,0 +1,31 @@
+//
+//  UIFont+.swift
+//  Elegant
+//
+//  Created by Steve on 2017/5/22.
+//  Copyright © 2017年 KingCQ. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: -  System fonts
+public extension UIFont {
+
+    public class var labelFont: UIFont {
+        return systemFont(ofSize: labelFontSize)
+    }
+
+    public class var buttonFont: UIFont {
+        return systemFont(ofSize: buttonFontSize)
+    }
+
+    public class var smallSystemFont: UIFont {
+        return systemFont(ofSize: smallSystemFontSize)
+    }
+
+    public class var systemFont: UIFont {
+        return systemFont(ofSize: systemFontSize)
+    }
+
+}
