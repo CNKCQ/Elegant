5528bb3bbd05c5b1dc7561065bbcb5197cfbf132
Steve <chengquan.wang@ele.me>
2017-06-10 00:00:38 +0800

    the different way for shuffle method

--- Elegant/Test.playground/Contents.swift
+++ Elegant/Test.playground/Contents.swift
@@ -5,3 +5,26 @@ import UIKit
 var str = "hello,playground"
 
 print(str.capitalized)
+
+func randomArray () {
+
+}
+
+extension Array {
+    var randomArrary: Array {
+        return sorted(by: {_,_ in arc4random() < arc4random()})
+    }
+
+    mutating func shuffle() -> Array {
+        indices.dropLast().forEach {
+            guard case let index = Int(arc4random_uniform(UInt32(count - $0))) + $0, index != $0 else { return }
+            self.swapAt($0, index)
+        }
+        return self
+    }
+}
+var arr = [1, 2, 3, 4]
+print(arr.randomArrary)
+print(arr.randomArrary)
+print(arr.shuffle())
+
