55f9d1aab41eec98b3c84eb3888ed9005be31b40
steve <chengquan.wang@ele.me>
2018-03-18 16:51:35 +0800

    gcd && lcm

--- Elegant/Test.playground/Contents.swift
+++ Elegant/Test.playground/Contents.swift
@@ -1,7 +1,22 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import Foundation
 
 var str = "hello,playground"
 
 print(str.capitalized)
+
+func gcd(a: TimeInterval, b: TimeInterval) ->  TimeInterval {
+    if (a == 0) {
+        return b
+    }
+    return gcd(a: fmod(b, a), b: a)
+}
+
+func lcm(a: TimeInterval, b: TimeInterval) -> TimeInterval {
+    return a*b/gcd(a: a, b: b)
+}
+print(gcd(a: 4, b: 6), lcm(a: 3, b: 6))
+
+
